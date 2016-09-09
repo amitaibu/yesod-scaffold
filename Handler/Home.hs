2c3b1c104bbd89c6e9014a953bc61b2be7b93ffa
Amitai Burstein <amitai@gizra.com>
2016-09-09 21:08:32 +0300

    Add attributes like the placeholder and CSS classes.

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -45,7 +45,18 @@ postHomeR = do
 sampleForm :: Form FileForm
 sampleForm = renderBootstrap3 BootstrapBasicForm $ FileForm
     <$> fileAFormReq "Choose a file"
-    <*> areq textField "What's on the file?" Nothing
+    <*> areq textField textSettings Nothing
+    -- Add attributes like the placeholder and CSS classes.
+    where textSettings = FieldSettings
+            { fsLabel = "What's on the file?"
+            , fsTooltip = Nothing
+            , fsId = Nothing
+            , fsName = Nothing
+            , fsAttrs =
+                [ ("class", "form-control")
+                , ("placeholder", "File description")
+                ]
+            }
 
 commentIds :: (Text, Text, Text)
 commentIds = ("js-commentForm", "js-createCommentTextarea", "js-commentList")
